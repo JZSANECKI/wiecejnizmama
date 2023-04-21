9030568ad9344ef194d5a9418e7ec362b1108189
Jakub Sanecki <j.z.sanecki@gmail.com>
2023-04-21 21:48:34 +0200

    mobile stuff

--- js/main.js
+++ js/main.js
@@ -13,7 +13,8 @@ const menuFn = () =>{
 menuBtn.addEventListener("click", menuFn)
 document.addEventListener("scroll", (event) => {
     if(window.scrollY > 0){
-    heroLogoMobile.style.width="15%"}else{
+    heroLogoMobile.style.width="15%" 
+}else{
         heroLogoMobile.style.width="45%"  
     }
 })
