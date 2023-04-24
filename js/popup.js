aaec6d0257d4efb3a61b0b8641c38669b7551e5a
Jakub Sanecki <j.z.sanecki@gmail.com>
2023-04-24 21:19:43 +0200

    dodany popup

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+let popupIndicator = localStorage.getItem('popup_shown')
+const popupModal = document.querySelector(".popup-canva")
+const closePopup =()=>{
+popupModal.classList.add("popup-canva-none")   
+}
+
+// if(popupIndicator === 'yes'){
+
+// }else{
+//     popupModal.classList.remove("popup-canva-none")
+//     localStorage.setItem('popup_shown','yes')
+//     popupIndicator = localStorage.getItem('popup_shown','yes')
+
+// }
+
